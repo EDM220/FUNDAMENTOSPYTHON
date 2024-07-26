af6ef05a4ff7206bf6c20026cf1d75f7e2b9fd34
Eduardo <edugamotac@hotmail.com>
2024-07-25 21:08:51 -0600

    cambios

---
+++ Cadenas.py
@@ -0,0 +1,53 @@
+# texto_variado ="Palabra 123 +-* #%&"
+# print(type(texto_variado))
+
+#Podemos usar comillas triples para que el texto se muestre como la cadena que hemos escrito
+# print('''
+# funcionamiento de 
+# programa: (opciones)    
+# -1 para acceder
+# -2 para salir''')
+
+# print('''
+# funcionamiento de \
+# programa: (opciones)    
+# -1 para acceder
+# -2 para salir''') # en este caso la "\" permite ver en pantalla el texto completa la línea funcionamiento y programa 
+
+# print('''
+# funcionamiento de \
+# programa: (opciones)    
+#      -1 para acceder
+#         -2 para salir''')
+
+#####################################################
+# suscriptin e indexado
+# texto='Python'
+# print(texto[0])
+# print(texto[5])
+# print(texto[-1])
+# print(texto[-6])
+
+# print(texto[6]) # esto produce error no se puede acceder a una posición que no existe
+# print(texto[-7]) # esto produce error no se puede acceder a una posición que no existe
+
+# letra = texto[-4]
+# print(letra)
+
+#texto[0] = 'p' # error no podemos modificar una cadena
+
+# letra = 'p' # Se puede redefinir la variable letra
+# print(letra)
+
+# texto_compuesto = letra + texto[1] #Concatenación
+# print(texto_compuesto)
+
+###########################################################
+
+
+
+
+
+
+
+
